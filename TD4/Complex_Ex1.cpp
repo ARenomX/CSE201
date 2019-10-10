ca8121ce0b95f69d210c373c8efba49d917727c2
andre.renom <andre.renom@polytechnique.edu>
2019-10-10 08:39:59 +0200

    Adding TD$ first version

---
+++ TD4/Complex_Ex1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+
+class Complex {
+private:
+  double real;        // the real part
+  double imaginary;   // the imaginary part
+
+  public:
+    // construct the complex number zero
+    void Complex(){
+      std::cout << 0 << std::endl;
+    }
+
+    // construct a complex number with given real and imaginary parts
+    void Complex(double real, double imaginary){
+      std::cout<<
+    }
+};
+
+int main{
+    return 0;
+}
