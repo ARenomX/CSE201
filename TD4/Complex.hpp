9959cfa1c252b7b25e61bc54c2b0c3e3a2f7713d
andre.renom <andre.renom@polytechnique.edu>
2019-10-10 09:44:38 +0200

    Final Version of split files for Exercise 3

---
+++ TD4/Complex.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include<cmath>
+
+class Complex {
+private:
+  double real;        // the real part
+  double imaginary;   // the imaginary part
+
+  public:
+    Complex();                    // construct the complex number zero
+    Complex(double r, double i);  // construct a complex number with given real and imaginary parts
+    double getReal();             // get the real part of the number
+    double getImaginary();        // get the imaginary part of the number
+    void setReal(double re);      // set the value of the real part
+    void setImaginary(double im); // set the value of the imaginary part
+    double magnitude();           // computes the distance from the complex origin
+/*
+   modifies the number so its magnitude becomes 1
+   but it stays in the same direction from the complex origin, if possible. In other
+   words, project the original number onto the unit circle. If that operation is not possible, the complex number must be left unchanged.
+*/
+    void normalize();
+    ~Complex(){std::cout << "Destroying " << real << " + " << imaginary << " * i." << std::endl;}
+};
